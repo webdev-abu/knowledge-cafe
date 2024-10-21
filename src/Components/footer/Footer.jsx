e7e8196702266185da3ac1282c36bec04bb343d5
abut40052@gmail.com <abut40052@gmail.com>
2024-10-21 22:39:22 +0530

    again add Footer UI Design

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -2,9 +2,9 @@
 export default function Footer() {
   return (
     <>
-      <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <footer className="footer footer-center text-base-content p-6 mt-8">
         <aside>
-            <p>Copyright © {new Date().getFullYear()} - All right reserved by ABU TAHIR</p>
+            <p className="sm:text-[13px] md:text-[16px] font-bold text-[#6047EC]">Copyright © {new Date().getFullYear()} - All right reserved by ABU TAHIR</p>
         </aside>
 </footer>
     </>
